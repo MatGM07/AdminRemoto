ae4a47a22aa87a611e7a8042f365ab0e25359985
Camilo <camilolon6@gmail.com>
2025-05-02 22:24:53 -0500

    Se eliminan clases HomeController y UsuarioController y los template html

--- src/main/java/com/admin/remoto/config/SecurityConfig.java
+++ src/main/java/com/admin/remoto/config/SecurityConfig.java
@@ -20,7 +20,6 @@ public class SecurityConfig {
         this.userDetailsService = userDetailsService;
     }
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
